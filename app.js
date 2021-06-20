cc468d9024ba0bb548b0e8dc31076562793a2c30
Gregory Bell <trihard.studios@outlook.com>
2021-06-20 17:39:14 -0600

    Added navigation and changed arrows

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-function fadeInImage(ref){
-    ref.classList.add("app_loaded_img");
+function fadeInImage(_ref){
+    _ref.classList.add("app_loaded_img");
 
+}
+
+
+function changePage(_page){
+    let pages = document.getElementsByTagName("body")[0].children;
+    for(let i = 0; i < pages.length; i++){
+        if(pages[i].classList.contains("page_active")) {
+            pages[i].classList.remove("page_active");
+            break;
+        }
+    }
+    _page.classList.add("page_active");
 }
